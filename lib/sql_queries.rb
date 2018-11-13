fb461e936ade8a2efead040c7310b968b60ef065
Dana Palmer <roiai4@hotmail.com>
2018-11-13 02:36:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,8 +10,8 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "SELECT name, age FROM bears WHERE alive = '1' ORDER BY age ASC;"
 end
 
-def selects_oldest_bear_and_returns_name_and_age("SELECT name, MAX(age) FROM bears;"")
-
+def selects_oldest_bear_and_returns_name_and_age
+"SELECT name, MAX(age) FROM bears;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
